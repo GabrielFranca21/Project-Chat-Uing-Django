2dd297c6f48d2650d0fe7fff7661d61848d3c191
Gabriel Franca <gabrielfrancasantana190504@gmail.com>
2024-01-18 17:06:49 -0500

    fisrt commit

---
+++ message_project/messaging/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+from .models import User, Message
+
+class ChatView(View):
+    template_name = 'messaging/chat.html'
+
+    def get(self, request):
+        users = User.objects.all()
+        messages = Message.objects.all()
+        return render(request, self.template_name, {'users': users, 'messages': messages})
